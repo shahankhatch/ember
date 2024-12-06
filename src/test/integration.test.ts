359e4eac1628756b60d0284283e3224a5629e2b6
Shahan K <shahan.k.code@gmail.com>
2024-12-06 02:27:10 -0500

    make things available

--- src/test/integration.test.ts
+++ src/test/integration.test.ts
@@ -3,16 +3,16 @@ import stylusAbi from "../../../ember-stylus/target/ember-stylus.json"
 import assert from "assert"
 
 const ARBITRUM_RPC_URL = "http://localhost:8547"
-const PRIVATE_KEY = "0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659"
+export const PRIVATE_KEY = "0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659"
 
-const provider = new ethers.JsonRpcProvider(ARBITRUM_RPC_URL)
-const wallet = new ethers.Wallet(PRIVATE_KEY, provider)
+export const provider = new ethers.JsonRpcProvider(ARBITRUM_RPC_URL)
+export const ownerWallet = new ethers.Wallet(PRIVATE_KEY, provider)
 
 // Address of your deployed Stylus contract
-const stylusAddress = "0xA6E41fFD769491a42A6e5Ce453259b93983a22EF"
+export const stylusAddress = "0xA6E41fFD769491a42A6e5Ce453259b93983a22EF"
 
 // Initialize the contract
-const stylusContract = new ethers.Contract(stylusAddress, stylusAbi, wallet)
+export const stylusContract = new ethers.Contract(stylusAddress, stylusAbi, ownerWallet)
 
 // Example function calls
 async function simulateSwap(swapValue: number) {
@@ -25,6 +25,12 @@ async function simulateSwap(swapValue: number) {
 export async function getIndex() {
     const result = await stylusContract.index()
     console.log(`Index:`, result.toString())
+    return result
+}
+
+export async function getVolatility() {
+    const result = await stylusContract.getVolatility()
+    console.log(`Volatility:`, result.toString())
     return result
 }
 
